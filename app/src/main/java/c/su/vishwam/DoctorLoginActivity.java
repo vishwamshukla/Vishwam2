abde6a6ed2c91b76f39b68452a17960450fd35d0
DELL <shuklavishwam21@gmail.com>
2019-09-04 23:43:31 +0530

    create settingactivity for doctors (another)

--- app/src/main/java/c/su/vishwam/DoctorLoginActivity.java
+++ app/src/main/java/c/su/vishwam/DoctorLoginActivity.java
@@ -1,18 +1,28 @@
 package c.su.vishwam;
 
+import android.app.ActivityOptions;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,13 +35,15 @@ import io.paperdb.Paper;
 
 public class DoctorLoginActivity extends AppCompatActivity {
 
-    private EditText InputPhoneNumber, InputPassword;
-    private Button LoginButton,LoginwithPhone;
+    private EditText InputEmail, InputPassword;
+    private Button LoginButton, LoginwithPhone;
     private ProgressDialog loadingBar;
     private String parentDbName = "Users";
     //private android.widget.CheckBox chkBoxRememberMe;
     private ImageView circle1;
     TextView tvLogin;
+    private ImageButton btRegister;
+    private FirebaseAuth mAuth;
 
     // private TextView ForgotPassword;
 
@@ -40,9 +52,13 @@ public class DoctorLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        btRegister = findViewById(R.id.btRegister);
+
         LoginButton = (Button) findViewById(R.id.login_btn_doctor);
         LoginwithPhone = (Button) findViewById(R.id.login_with_phone_doctor);
-        InputPhoneNumber = (EditText) findViewById(R.id.login_phone_number_input_doctor);
+        InputEmail = (EditText) findViewById(R.id.login_email_input_doctor);
         InputPassword = (EditText) findViewById(R.id.login_password_input_doctor);
         loadingBar = new ProgressDialog(this);
         //chkBoxRememberMe = (CheckBox) findViewById(R.id.remember_me_chkb_reception);
@@ -57,77 +73,123 @@ public class DoctorLoginActivity extends AppCompatActivity {
             }
         });
 
-
-    }
-    private void LoginUser() {
-        String phone = InputPhoneNumber.getText().toString();
-        String password = InputPassword.getText().toString();
-
-        if (TextUtils.isEmpty(phone)){
-            Toast.makeText(this, "Enter Phone Number!", Toast.LENGTH_SHORT).show();
-        }
-        else if (TextUtils.isEmpty(password)){
-            Toast.makeText(this, "Enter Password!", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            loadingBar.setTitle("Logging in....");
-            loadingBar.setMessage("Please wait, checking credentials...");
-            loadingBar.setCanceledOnTouchOutside(false);
-            loadingBar.show();
-
-            AllowAccessToAccount(phone,password);
-        }
-    }
-
-    private void AllowAccessToAccount(final String phone, final String password) {
-
-//        if (chkBoxRememberMe.isChecked()){
-//            Paper.book().write(Prevalent.UserPhoneKey,phone);
-//            Paper.book().write(Prevalent.UserPasswordKey,password);
-//        }
-
-
-        final DatabaseReference RootRef;
-        RootRef = FirebaseDatabase.getInstance().getReference();
-
-        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+        btRegister.setOnClickListener(new View.OnClickListener() {
+            // @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.child(parentDbName).child(phone).exists()){
-                    Users usersData = dataSnapshot.child(parentDbName).child(phone).getValue(Users.class);
-                    if (usersData.getPhone().equals(phone)){
-                        if (usersData.getPassword().equals(password)){
-//                            if (parentDbName.equals("Admins")){
-//                                Toast.makeText(LoginActivity.this, "Logged in!", Toast.LENGTH_SHORT).show();
-//                                loadingBar.dismiss();
-//
-//                                Intent intent = new Intent(LoginActivity.this,AdminCategoryActivity.class);
-//                                startActivity(intent);
-//                            }
-                            if (parentDbName.equals("Users")){
-                                Toast.makeText(DoctorLoginActivity.this, "Logged in!", Toast.LENGTH_SHORT).show();
-                                loadingBar.dismiss();
-
-                                Intent intent = new Intent(DoctorLoginActivity.this,HomeActivity.class);
-                                Prevalent.currentOnlineUser = usersData;
-                                startActivity(intent);
-                            }                        }
-                        else {
-                            loadingBar.dismiss();
-                            Toast.makeText(DoctorLoginActivity.this, "Invalid Password", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }
-                else {
-                    Toast.makeText(DoctorLoginActivity.this, "Account with this "+phone+" number do not exists", Toast.LENGTH_SHORT).show();
-                    loadingBar.dismiss();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
+            public void onClick(View v) {
+//                if (v==btRegister){
+//                    Intent a = new Intent(DoctorLoginActivity.this, DoctorRegisterActivity.class);
+//                    Pair[] pairs = new Pair[1];
+//                    pairs[0] = new Pair<View,String> (tvLogin,"login");
+//                    ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(DoctorLoginActivity.this,pairs);
+//                    startActivity(a,activityOptions.toBundle());
+//                }
+                startActivity(new Intent(DoctorLoginActivity.this, DoctorRegisterActivity.class));
             }
         });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser ==  null){
+            SendUserToHomeActivity();
+        }
+    }
+
+    private void LoginUser() {
+        String email = InputEmail.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Enter Phone Number!", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Enter Password!", Toast.LENGTH_SHORT).show();
+        } else {
+            loadingBar.setTitle("Login in progress");
+            loadingBar.setMessage("Please wait...");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                SendUserToHomeActivity();
+                                Toast.makeText(DoctorLoginActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            } else {
+                                String message = task.getException().toString();
+                                Toast.makeText(DoctorLoginActivity.this, "Error-" + message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void SendUserToHomeActivity() {
+        Intent intent = new Intent(DoctorLoginActivity.this, HomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
+
+//    private void AllowAccessToAccount(final String email, final String password) {
+//
+////        if (chkBoxRememberMe.isChecked()){
+////            Paper.book().write(Prevalent.UserPhoneKey,phone);
+////            Paper.book().write(Prevalent.UserPasswordKey,password);
+////        }
+//
+//
+//        final DatabaseReference RootRef;
+//        RootRef = FirebaseDatabase.getInstance().getReference();
+//
+//        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                if (dataSnapshot.child(parentDbName).child(phone).exists()){
+//                    Users usersData = dataSnapshot.child(parentDbName).child(phone).getValue(Users.class);
+//                    if (usersData.getPhone().equals(phone)){
+//                        if (usersData.getPassword().equals(password)){
+////                            if (parentDbName.equals("Admins")){
+////                                Toast.makeText(LoginActivity.this, "Logged in!", Toast.LENGTH_SHORT).show();
+////                                loadingBar.dismiss();
+////
+////                                Intent intent = new Intent(LoginActivity.this,AdminCategoryActivity.class);
+////                                startActivity(intent);
+////                            }
+//                            if (parentDbName.equals("Users")){
+//                                Toast.makeText(DoctorLoginActivity.this, "Logged in!", Toast.LENGTH_SHORT).show();
+//                                loadingBar.dismiss();
+//
+//                                Intent intent = new Intent(DoctorLoginActivity.this,HomeActivity.class);
+//                                Prevalent.currentOnlineUser = usersData;
+//                                startActivity(intent);
+//                            }                        }
+//                        else {
+//                            loadingBar.dismiss();
+//                            Toast.makeText(DoctorLoginActivity.this, "Invalid Password", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                }
+//                else {
+//                    Toast.makeText(DoctorLoginActivity.this, "Account with this "+phone+" number do not exists", Toast.LENGTH_SHORT).show();
+//                    loadingBar.dismiss();
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
+//}
---
+++ app/src/main/java/c/su/vishwam/DoctorRegisterActivity.java
@@ -0,0 +1,123 @@
+package c.su.vishwam;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class DoctorRegisterActivity extends AppCompatActivity {
+
+    private RelativeLayout rlayout;
+    private Animation animation;
+    private Menu menu;
+
+    private EditText UserEmail, UserPassword, UserConfirmPassword;
+    private Button CreateAccountButton;
+    private ProgressDialog loadingBar;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
+
+        rlayout     = findViewById(R.id.rlayout);
+        animation   = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        rlayout.setAnimation(animation);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loadingBar = new ProgressDialog(this);
+
+        UserEmail = findViewById(R.id.doctor_email);
+        UserPassword = findViewById(R.id.doctor_password);
+        UserConfirmPassword =  findViewById(R.id.doctor_confirm_password);
+        CreateAccountButton =  findViewById(R.id.register_create_account);
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateNewAccount();
+            }
+        });
+    }
+    private void CreateNewAccount() {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+        String confirmPassword = UserConfirmPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Email required", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Password required", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(confirmPassword)){
+            Toast.makeText(this, "Confirm the password", Toast.LENGTH_SHORT).show();
+        }
+        else if(!password.equals(confirmPassword)){
+            Toast.makeText(this, "Password do not match", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            loadingBar.setTitle("Creating Account");
+            loadingBar.setMessage("Please wait...");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                SendUserToSetupActivity();
+                                Toast.makeText(DoctorRegisterActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                            else{
+                                String message = task.getException().getMessage();
+                                Toast.makeText(DoctorRegisterActivity.this, "Error-> "+message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void SendUserToSetupActivity() {
+        Intent intent = new Intent(DoctorRegisterActivity.this, SettingsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
