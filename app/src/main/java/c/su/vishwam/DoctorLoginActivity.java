60d1def834209b1163a14a036cbb926f4eb7bbe3
DELL <shuklavishwam21@gmail.com>
2019-09-06 00:06:23 +0530

    create settingactivity for doctors (another)

--- app/src/main/java/c/su/vishwam/DoctorLoginActivity.java
+++ app/src/main/java/c/su/vishwam/DoctorLoginActivity.java
@@ -5,10 +5,13 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import com.google.android.gms.auth.api.Auth;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
@@ -18,11 +21,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,7 +50,7 @@ public class DoctorLoginActivity extends AppCompatActivity {
 
     private EditText InputEmail, InputPassword;
     private TextView forgotPassword;
-    private Button LoginButton, LoginwithPhone;
+    private Button LoginButton, LoginwithGoogle;
     private ProgressDialog loadingBar;
     private String parentDbName = "Users";
     //private android.widget.CheckBox chkBoxRememberMe;
@@ -45,6 +58,10 @@ public class DoctorLoginActivity extends AppCompatActivity {
     TextView tvLogin;
     private ImageButton btRegister;
     private FirebaseAuth mAuth;
+    private static final String TAG = "DoctorLoginActivity";
+
+    private static final int RC_SIGN_IN = 1;
+    private GoogleApiClient mGoogleSignInClient;
 
     // private TextView ForgotPassword;
 
@@ -60,7 +77,7 @@ public class DoctorLoginActivity extends AppCompatActivity {
         forgotPassword = (TextView) findViewById(R.id.tvForgot);
 
         LoginButton = (Button) findViewById(R.id.login_btn_doctor);
-        LoginwithPhone = (Button) findViewById(R.id.login_with_phone_doctor);
+        LoginwithGoogle = (Button) findViewById(R.id.login_with_google);
         InputEmail = (EditText) findViewById(R.id.login_email_input_doctor);
         InputPassword = (EditText) findViewById(R.id.login_password_input_doctor);
         loadingBar = new ProgressDialog(this);
@@ -98,7 +115,82 @@ public class DoctorLoginActivity extends AppCompatActivity {
             }
         });
 
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(DoctorLoginActivity.this, "Something went wrong...", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        LoginwithGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
     }
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleSignInClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            //GoogleSignInResult result = new Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+                            startActivity(new Intent(DoctorLoginActivity.this, DoctorProfileActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(DoctorLoginActivity.this, "Something went wrong...", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(DoctorLoginActivity.this, DoctorLoginActivity.class));
+//                            Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 
     @Override
     protected void onStart() {
