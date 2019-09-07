754fe69302c00c10a422fbf58c0d68a214ff26b4
DELL <shuklavishwam21@gmail.com>
2019-09-07 19:23:43 +0530

    User scenes reception

--- app/src/main/java/c/su/vishwam/NurseHomeActivity.java
+++ app/src/main/java/c/su/vishwam/NurseHomeActivity.java
@@ -1,13 +1,35 @@
 package c.su.vishwam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class NurseHomeActivity extends AppCompatActivity {
+
+    ImageView AddOpd, Profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nurse_home);
+        setContentView(R.layout.activity_reception);
+
+        AddOpd = (ImageView) findViewById(R.id.reception_add_opd);
+
+        Profile = (ImageView) findViewById(R.id.reception_profile);
+
+//        AddOpd.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(ReceptionActivity.this, Reception_add_opdActivity.class));
+//            }
+//        });
+        Profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(NurseHomeActivity.this, NurseProfileActivity.class));
+            }
+        });
     }
 }
