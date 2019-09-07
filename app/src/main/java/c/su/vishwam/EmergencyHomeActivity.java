54366feed361b0ef7eb8713a090ee15c03acd02e
DELL <shuklavishwam21@gmail.com>
2019-09-07 23:21:48 +0530

    User scenes pharmacy

--- app/src/main/java/c/su/vishwam/EmergencyHomeActivity.java
+++ app/src/main/java/c/su/vishwam/EmergencyHomeActivity.java
@@ -1,13 +1,26 @@
 package c.su.vishwam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class EmergencyHomeActivity extends AppCompatActivity {
 
+    private ImageView profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_home);
+
+        profile = findViewById(R.id.reception_profile);
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EmergencyHomeActivity.this, EmergencyProfileActivity.class));
+            }
+        });
     }
 }
